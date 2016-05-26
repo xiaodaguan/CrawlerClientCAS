8d9670ff1bd98655884ee33a57f27f373e09b3ca
gxd <xiaodaguan@gmail.com>
2016-05-26 16:08:45 +0800

    修复bug: pubtime格式为"今天:xx:xx"的时间解析错误的问题

--- src/common/extractor/xpath/weibo/search/sub/SinaExtractor.java
+++ src/common/extractor/xpath/weibo/search/sub/SinaExtractor.java
@@ -17,31 +17,27 @@ import java.util.List;
 
 public class SinaExtractor extends WeiboSearchXpathExtractor {
 
-	@Override
-	protected Node getRealDOM(HtmlInfo html) throws SAXException, IOException {
-		String content = html.getContent();
-		if (content == null)
-			return null;
+    @Override
+    protected Node getRealDOM(HtmlInfo html) throws SAXException, IOException {
+        String content = html.getContent();
+        if (content == null) return null;
 
-		if (html.getType().contains("META/")) {
-			String temp = StringUtil.regMatcher(content,
-					"<script>STK && STK.pageletM && STK.pageletM.view\\(\\{\"pid\":\"pl_weibo_direct\",", "\\)</script>");
-			if (temp != null) {
-				content = "{" + temp;
-				content = JsonUtil.getStringByKey(content, "html");
-			}
-		}
-		else if(html.getType().contains("DATA/")){
-			
-		}
-		else {
-			Systemconfig.sysLog.log("页面dom解析异常，请查看！");
-		}
+        if (html.getType().contains("META/")) {
+            String temp = StringUtil.regMatcher(content, "<script>STK && STK.pageletM && STK.pageletM.view\\(\\{\"pid\":\"pl_weibo_direct\",", "\\)</script>");
+            if (temp != null) {
+                content = "{" + temp;
+                content = JsonUtil.getStringByKey(content, "html");
+            }
+        } else if (html.getType().contains("DATA/")) {
 
-		html.setContent(content);
+        } else {
+            Systemconfig.sysLog.log("页面dom解析异常，请查看！");
+        }
 
-		return super.getRealDOM(html);
-	}
+        html.setContent(content);
+
+        return super.getRealDOM(html);
+    }
 
 //	@Override
 //	public void parseUid(List<WeiboData> list, Node domtree, Component component, String... args) {
@@ -77,19 +73,19 @@ public class SinaExtractor extends WeiboSearchXpathExtractor {
 //
 //	}
 
-	@Override
-	public void parseCommenturl(List<WeiboData> list, Node dom, Component component, String... args) {
-		for (int i = 0; i < list.size(); i++) {
-			list.get(i).setCommentUrl("http://weibo.com/aj/comment/big?id=" + list.get(i).getMid());
-		}
-	}
+    @Override
+    public void parseCommenturl(List<WeiboData> list, Node dom, Component component, String... args) {
+        for (int i = 0; i < list.size(); i++) {
+            list.get(i).setCommentUrl("http://weibo.com/aj/comment/big?id=" + list.get(i).getMid());
+        }
+    }
 
-	@Override
-	public void parseRtturl(List<WeiboData> list, Node dom, Component component, String... args) {
-		for (int i = 0; i < list.size(); i++) {
-			list.get(i).setRttUrl("http://weibo.com/aj/mblog/info/big?id=" + list.get(i).getMid());
-		}
-	}
+    @Override
+    public void parseRtturl(List<WeiboData> list, Node dom, Component component, String... args) {
+        for (int i = 0; i < list.size(); i++) {
+            list.get(i).setRttUrl("http://weibo.com/aj/mblog/info/big?id=" + list.get(i).getMid());
+        }
+    }
 
 //	@Override
 //	public void parsePubtime(List<WeiboData> list, Node dom, Component component, String... args) {
@@ -101,39 +97,41 @@ public class SinaExtractor extends WeiboSearchXpathExtractor {
 //	}
 
     @Override
-	public Date timeProcess(String s) {
-		Date d = super.timeProcess(s);
-		if (d == null) {
-			Calendar c = Calendar.getInstance();
-			if (s.indexOf("月") > -1 || s.indexOf("日") > -1) {
-				if (s.indexOf("年") == -1) {
-					s = c.get(Calendar.YEAR) + "-" + s;
-				}
-				s = s.replace("年", "-").replace("月", "-").replace("日", "");
-				d = super.timeProcess(s);
-			}
-			if (d == null) {
+    public Date timeProcess(String s) {
+        Date d = super.timeProcess(s);
+        if (d == null) {
+            Calendar c = Calendar.getInstance();
+            if (s.indexOf("月") > -1 || s.indexOf("日") > -1) {
+                if (s.indexOf("年") == -1) {
+                    s = c.get(Calendar.YEAR) + "-" + s;
+                }
+                s = s.replace("年", "-").replace("月", "-").replace("日", "");
+                d = super.timeProcess(s);
+            }
+            if (d == null) {
 
-				int num = Integer.parseInt(StringUtil.extrator(s, "\\d"));
-				if (s.contains("minute") || s.contains("分钟前")) {
-					c.set(Calendar.MINUTE, c.get(Calendar.MINUTE) - num);
-				} else if (s.contains("hour") || s.contains("小时前")) {
-					c.set(Calendar.HOUR, c.get(Calendar.HOUR) - num);
-				} else if (s.contains("今天")) {
-					s.replace("今天", c.get(Calendar.YEAR) + "-" + c.get(Calendar.MONTH) + "-" + c.get(Calendar.DATE) + "");
-				} else if (s.contains("day") || s.contains("天前")) {
-					c.set(Calendar.DAY_OF_MONTH, c.get(Calendar.DAY_OF_MONTH) - num);
-				} else if (s.contains("month") || s.contains("月前")) {
-					c.set(Calendar.MONTH, c.get(Calendar.MONTH) - num);
-				} else if (s.contains("year") || s.contains("年前")) {
-					c.set(Calendar.YEAR, c.get(Calendar.YEAR) - num);
-				} else if (s.contains("second") || s.contains("秒前")) {
-					c.set(Calendar.SECOND, c.get(Calendar.SECOND) - num);
-				}
-				return c.getTime();
-			}
-		}
-		return d;
-	}
+                int num = Integer.parseInt(StringUtil.extrator(s, "\\d"));
+                if (s.contains("minute") || s.contains("分钟前")) {
+                    c.set(Calendar.MINUTE, c.get(Calendar.MINUTE) - num);
+                } else if (s.contains("hour") || s.contains("小时前")) {
+                    c.set(Calendar.HOUR, c.get(Calendar.HOUR) - num);
+                } else if (s.contains("今天")) {
+                    s = s.replace("今天", c.get(Calendar.YEAR) + "-" + c.get(Calendar.MONTH) + "-" + c.get(Calendar.DATE) + "");
+                    d = super.timeProcess(s);
+                    return d;
+                } else if (s.contains("day") || s.contains("天前")) {
+                    c.set(Calendar.DAY_OF_MONTH, c.get(Calendar.DAY_OF_MONTH) - num);
+                } else if (s.contains("month") || s.contains("月前")) {
+                    c.set(Calendar.MONTH, c.get(Calendar.MONTH) - num);
+                } else if (s.contains("year") || s.contains("年前")) {
+                    c.set(Calendar.YEAR, c.get(Calendar.YEAR) - num);
+                } else if (s.contains("second") || s.contains("秒前")) {
+                    c.set(Calendar.SECOND, c.get(Calendar.SECOND) - num);
+                }
+                return c.getTime();
+            }
+        }
+        return d;
+    }
 
 }
