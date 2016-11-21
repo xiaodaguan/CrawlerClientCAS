20e405df0dfbcf44afef3f67b11d9e73acfc522e
gxd <xiaodaguan@gmail.com>
2016-11-21 15:53:04 +0800

    [update]phantomjs更换了调用，使用jBrowserDriver，感谢

--- src/common/util/SeleniumDriverManager.java
+++ src/common/util/SeleniumDriverManager.java
@@ -4,6 +4,9 @@ package common.util;
  * Created by guanxiaoda on 16/8/10.
  */
 
+import com.machinepublishers.jbrowserdriver.JBrowserDriver;
+import com.machinepublishers.jbrowserdriver.Settings;
+import com.machinepublishers.jbrowserdriver.Timezone;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 
 import java.awt.*;
@@ -20,30 +23,12 @@ public class SeleniumDriverManager {
     private SeleniumDriverManager() {
     }
 
-    public PhantomJSDriver getPhantomJSDriver() throws Exception {
-        String os = System.getProperty("os.name").toLowerCase();//linux, windows xx, mac os x
-
-        if (os.contains("mac")) System.setProperty("phantomjs.binary.path", "phantomjs/osx/phantomjs");
-        else if (os.contains("windows")) System.setProperty("phantomjs.binary.path", "phantomjs/win/phantomjs.exe");
-        else if (os.contains("linux")) {
-            String version = System.getProperty("os.arch").toLowerCase();
-            if (version.contains("64")) System.setProperty("phantomjs.binary.path", "phantomjs/linux/64/phantomjs");
-            else System.setProperty("phantomjs.binary.path", "phantomjs/linux/32/phantomjs");
-        }
+    public JBrowserDriver getPhantomJSDriver() throws Exception {
 
 
-        PhantomJSDriver driver = new PhantomJSDriver();
+        JBrowserDriver driver = new JBrowserDriver(Settings.builder().timezone(Timezone.ASIA_SHANGHAI).build());
         return driver;
-//        DesiredCapabilities caps = new DesiredCapabilities();
-//        caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "phantomjs/osx/phantomjs");
-//        PhantomJSDriver driver = null;
-//        try {
-//            driver = new PhantomJSDriver(caps);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        driver.manage().window().setSize(new Dimension(1920, 1080));
-//        return driver;
+
     }
 
 
