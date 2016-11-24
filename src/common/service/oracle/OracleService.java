1085723d771c89148945aebb9605332bf5c8b4f7
gxd <xiaodaguan@gmail.com>
2016-11-24 15:24:10 +0800

    bug fix

--- src/common/service/oracle/OracleService.java
+++ src/common/service/oracle/OracleService.java
@@ -100,7 +100,6 @@ public abstract class OracleService<T> extends AbstractDBService<T> {
             case 3:
             case 5:
             case 7:
-            case 9:
             case 11:
             case 13:
             case 15:
