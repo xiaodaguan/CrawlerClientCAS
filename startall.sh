44fb96d374423a61bb507da6c029579eb32f569a
gxd <xiaodaguan@gmail.com>
2016-12-06 15:19:53 +0800

    add exec .sh

--- startall.sh
+++ startall.sh
@@ -1,7 +1,7 @@
-java -jar CrawlerClientCAS.jar type=1 name=topsearch_crawlerclient project=5
-java -jar CrawlerClientCAS.jar type=2 name=topsearch_crawlerclient project=5
-java -jar CrawlerClientCAS.jar type=3 name=topsearch_crawlerclient project=5
-java -jar CrawlerClientCAS.jar type=5 name=topsearch_crawlerclient project=5
-java -jar CrawlerClientCAS.jar type=7 name=topsearch_crawlerclient project=5
-java -jar CrawlerClientCAS.jar type=8 name=topsearch_crawlerclient project=5
-java -jar CrawlerClientCAS.jar type=15 name=topsearch_crawlerclient project=5
+nohup java -jar CrawlerClientCAS.jar type=1 name=topsearch_crawlerclient project=5 > newss.log&
+nohup java -jar CrawlerClientCAS.jar type=2 name=topsearch_crawlerclient project=5 > newsm.log&
+nohup java -jar CrawlerClientCAS.jar type=3 name=topsearch_crawlerclient project=5 > bbss.log&
+nohup java -jar CrawlerClientCAS.jar type=5 name=topsearch_crawlerclient project=5 > blogs.log&
+nohup java -jar CrawlerClientCAS.jar type=7 name=topsearch_crawlerclient project=5 > weibos.log&
+nohup java -jar CrawlerClientCAS.jar type=8 name=topsearch_crawlerclient project=5 > weibom.log&
+nohup java -jar CrawlerClientCAS.jar type=15 name=topsearch_crawlerclient project=5 > weixins.log&
