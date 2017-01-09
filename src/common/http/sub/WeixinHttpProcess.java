0a532c64d561699fb08faea0028096c38b296210
gxd <xiaodaguan@gmail.com>
2017-01-09 15:48:04 +0800

    weixin js again...

--- src/common/http/sub/WeixinHttpProcess.java
+++ src/common/http/sub/WeixinHttpProcess.java
@@ -15,7 +15,25 @@ public class WeixinHttpProcess extends SimpleHttpProcess {
     public synchronized byte[] simpleGet(HtmlInfo html) throws Exception {
 
         Runtime runtime = Runtime.getRuntime();
-        Process p = runtime.exec("phantomjs/drivers/osx/phantomjs phantomjs/get.js "+html.getOrignUrl());
+        Process p = null;
+
+
+        String os = System.getProperty("os.name").toLowerCase();//linux, windows xx, mac os x
+
+
+        if (os.contains("mac")) runtime.exec("phantomjs/drivers/osx/phantomjs phantomjs/get.js "+html.getOrignUrl());
+        else if (os.contains("windows"))
+            runtime.exec("phantomjs/drivers/win/phantomjs.exe phantomjs/get.js "+html.getOrignUrl());
+        else if (os.contains("linux")) {
+            String version = System.getProperty("os.arch").toLowerCase();
+            if (version.contains("64"))
+                runtime.exec("phantomjs/drivers/linux/64/phantomjs phantomjs/get.js "+html.getOrignUrl());
+            else
+                runtime.exec("phantomjs/drivers/linux/32/phantomjs phantomjs/get.js "+html.getOrignUrl());
+        }
+
+
+
         InputStream is = p.getInputStream();
         BufferedReader br = new BufferedReader(new InputStreamReader(is));
         String line = null;
