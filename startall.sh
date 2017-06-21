b5ca7a63aed714c97b1bda7a6df9e7b100ff8b6f
gxd <xiaodaguan@gmail.com>
2017-06-21 15:29:26 +0800

    修改启动脚本1

--- startall.sh
+++ startall.sh
@@ -1,5 +1,5 @@
-nohup java -jar target/CrawlerClientCAS-1.0.jar.jar type=1 name=crawlerclient project=3 > newss.log&
-nohup java -jar target/CrawlerClientCAS-1.0.jar.jar type=2 name=crawlerclient project=3 > newsm.log&
-nohup java -jar target/CrawlerClientCAS-1.0.jar.jar type=3 name=crawlerclient project=3 > bbss.log&
-nohup java -jar target/CrawlerClientCAS-1.0.jar.jar type=7 name=crawlerclient project=3 > weibos.log&
-nohup java -jar target/CrawlerClientCAS-1.0.jar.jar type=15 name=crawlerclient project=3 > weixins.log&
+nohup java -jar target/CrawlerClientCAS-1.0jar type=1 name=crawlerclient project=3 > newss.log&
+nohup java -jar target/CrawlerClientCAS-1.0jar type=2 name=crawlerclient project=3 > newsm.log&
+nohup java -jar target/CrawlerClientCAS-1.0jar type=3 name=crawlerclient project=3 > bbss.log&
+nohup java -jar target/CrawlerClientCAS-1.0jar type=7 name=crawlerclient project=3 > weibos.log&
+nohup java -jar target/CrawlerClientCAS-1.0jar type=15 name=crawlerclient project=3 > weixins.log&
