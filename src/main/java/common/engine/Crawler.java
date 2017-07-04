16643bd2fd6967868651897a44af6c17da564d0a
xiaodaguan <xiaodaguan@gmail.com>
2017-07-04 10:09:50 +0800

    scheduler 完成

---
+++ src/main/java/common/engine/Crawler.java
@@ -0,0 +1,20 @@
+package common.engine;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by guanxiaoda on 2017/7/4.
+ */
+public class Crawler {
+    public static final Logger LOGGER = LoggerFactory.getLogger(Crawler.class);
+
+    public Crawler(String crawlerName, int mediaType){
+
+    }
+
+
+    public void start(){
+
+    }
+}
